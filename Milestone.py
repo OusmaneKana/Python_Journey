1a010750bb42798ab1179524163db9404b0c5a7e
root <kanaman6@gmail.com>
2018-02-18 23:58:34 -0600

    skelet ready!

--- Milestone.py
+++ Milestone.py
@@ -1,1 +1,30 @@
 #My first milestone project
+from __future__ import print_function
+
+from IPython.display import clear_output
+def display_board(board):
+	pass
+
+def player_input():
+	pass
+
+def place_marker(board, marker, position):
+	pass
+
+def win_check(board, mark):
+	pass
+
+import random
+def space_check(board, position):
+	pass
+
+def full_board_check(board):
+	pass
+
+def player_choice(board):
+	pass
+
+def replay():
+	pass
+
+print('Welcome to Tic Tac Toe!')
