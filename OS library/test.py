62cbc2083ca4a826d98d068ff9c833d64d9ddc7e
OusmaneKana <kanaman6@gmail.com>
2020-01-24 17:54:44 -0600

    Automating boring stuffs: 	Making us of the OS library to automate my study process. 	By making a little program that asks what task am I going to do 	(eg studying physic). It is then going to open all the web pages 	and local documents and appliction I might need

---
+++ OS library/test.py
@@ -0,0 +1,10 @@
+import os 
+import webbrowser
+
+print("This a small little program that will help me organize myself \n And whenever I open my laptop I will not have to open the same things \n again and again")
+choice = input("What are you studying today ?")
+if choice == "1":
+	webbrowser.open("https://eagleonline.hccs.edu/courses/110128")
+	os.system('start C:"\"Users"\"Serigne"\"OneDrive - Houston Community College"\"Spring 2020')
+
+
